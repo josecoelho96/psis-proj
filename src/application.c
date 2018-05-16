09408e2adf2785b3aa0738d6cefeb196f9e4a8e1
José Coelho <josecoelho96@gmail.com>
2018-05-16 10:29:39 +0100

    Update gitignore

---
+++ src/application.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    printf("Application test!\n");
+    return 0;
+}
---
+++ src/clipboard.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    printf("Clipboard test!\n");
+    return 0;
+}
